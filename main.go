e1781328bd63a3206103b3c23f1948906f9de65c
Philippe DeSousa <phil.desousa@gmail.com>
2025-04-27 21:30:24 -0400

    remove secretsmanager code

--- main.go
+++ main.go
@@ -5,11 +5,9 @@ import (
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 )
 
 var db *dynamodb.Client
-var sm *secretsmanager.Client
 
 func init() {
 	dbClient, err := getDbClient()
@@ -18,13 +16,6 @@ func init() {
 		log.Fatal(err)
 	}
 	db = dbClient
-
-	smClient, err := getSmClient()
-	if err != nil {
-		log.Println("init() error running getSecretsManagerClient(): ")
-		log.Fatal(err)
-	}
-	sm = smClient
 }
 
 func main() {
